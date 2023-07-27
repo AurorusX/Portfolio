81a352b1940083dd7288dcc82cdec0b969383c12
Shane Okukenu <shane.okukenu@MCSYSTEMS.COM>
2023-07-26 23:10:20 -0500

    finalize skills section design

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -42,9 +42,10 @@ const Skills = () => {
  <>
  <h2 className='head-text'>Skills & Experience</h2>
  <div className='app__skills-container'>
+
   <motion.div className='app__skills-list'>
    {
-      skills.map((skill)=>(
+      skills?.map((skill)=>(
         <motion.div
         whileInView={{opacity:[0,1]}}
         transition={{ duration : 0.5}}
@@ -63,6 +64,65 @@ const Skills = () => {
    }
 
 
+  </motion.div>
+
+  <motion.div className='app__skills-exp'>
+
+    {experience?.map((experience)=>(
+     <motion.div
+     className='app__skills-exp-item'
+     key={experience.year}     
+     >
+      <div className='app__skills-exp-year'>
+
+        <p className='bold-text'>{experience.year}</p>
+
+
+      </div>
+      <motion.div
+      className='app__skills-exp-works'
+
+      >
+        {experience.works.map((work) =>(
+
+              <>
+              <motion.div
+                  whileInView={{opacity:[0,1]}}
+                  transition={{ duration : 0.5}}
+                  className='app__skills-exp-work'
+                  key={work.name}
+                  data-tip
+                  data-for={work.name}
+
+              >
+
+                <h4 className='bold-text'>{work.name}</h4>
+                <p className='p-text'>{work.company}   </p>
+
+              </motion.div>
+              <ReactTooltip
+              id={work.name}
+              effect='solid'
+              arrowColor='#fff'
+              className='skills-tooltip'
+
+              >
+                  {work.desc}
+              </ReactTooltip>
+              </>
+
+
+        ))}
+
+
+      </motion.div>
+
+
+     </motion.div>
+
+
+    ))}
+      
   </motion.div>
 
 
